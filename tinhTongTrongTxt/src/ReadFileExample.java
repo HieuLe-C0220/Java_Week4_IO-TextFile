46d61e1238bdf4557df331b27ad2d1d6b426f76e
Hieu.Le <letrunghieu2602@gmail.com>
2020-05-04 09:54:08 +0700

    làm lại

---
+++ tinhTongTrongTxt/src/ReadFileExample.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            //Đọc file theo đường dẫn
+            File file = new File(filePath);
+            //Kiểm tra file tồn tại hay không
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            //Đọc từng dòng của file và tiến hành cộng tổng lại
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            String line ="";
+            int sum = 0;
+            while ((line = br.readLine())!=null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tổng = "+sum);
+        } catch (Exception e) {
+            //TH file không tồn tại hoặc nội dung file có lỗi thì sẽ hiển thị thông báo lỗi
+            System.out.println("File không tồn tại hoặc nội dung có lỗi!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        ReadFileExample readFileEx = new ReadFileExample();
+        readFileEx.readFileText(path);
+    }
+}
